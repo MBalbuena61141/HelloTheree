96b49e631fd0b9f21f8b395afe38ad9acaafb205
62063 <jgurung.62063@farnborough.ac.uk>
2017-09-14 16:11:08 +0100

    i did somting

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hi hi hi!");
+        System.out.println("Hi hi hi!"); //<--- this is noooo good
+        System.out.println("Bye Bye Bye!"); //<----- this is good
+        
+
     }
 }
